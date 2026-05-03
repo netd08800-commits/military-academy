4925849a048ea24609f8495daf3ce5657e0ee025
netd08800-commits <netd08800@gmail.com>
2026-05-03 20:08:12 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js - НЕ загружайте этот файл в публичный репозиторий!
-export const SUPABASE_CONFIG = {
-    url: 'https://innljjhvmyzgfqhenmjq.supabase.co',
-    anonKey: 'sb_publishable_ViVCLNkbxhKos0sVCWlcOA_eZQEDDO3'
-};
