ef2e441fe634d81674ec825e6fcc2403ce11b0b9
netd08800-commits <netd08800@gmail.com>
2026-05-03 06:35:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - НЕ загружайте этот файл в публичный репозиторий!
 export const SUPABASE_CONFIG = {
-    url: 'https://YOUR_PROJECT_ID.supabase.co',
-    anonKey: 'YOUR_ANON_KEY'
+    url: 'https://innljjhvmyzgfqhenmjq.supabase.co',
+    anonKey: 'sb_publishable_ViVCLNkbxhKos0sVCWlcOA_eZQEDDO3'
 };
